551c91d4badbe46c366628dfa5fb2bb9f1339505
CokeLee777 <akak640@kyonggi.ac.kr>
2026-01-09 19:43:06 +0900

    [FEAT] init project

---
+++ src/main/java/me/cokelee777/kafkalargepayloadprocessor/KafkaLargePayloadProcessorApplication.java
@@ -0,0 +1,13 @@
+package me.cokelee777.kafkalargepayloadprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLargePayloadProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaLargePayloadProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cokelee777/kafkalargepayloadprocessor/KafkaLargePayloadProcessorApplicationTests.java
@@ -0,0 +1,14 @@
+package me.cokelee777.kafkalargepayloadprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class KafkaLargePayloadProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
